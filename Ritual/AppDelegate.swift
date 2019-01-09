45f6a60af1539060b41ce6f6ba1bd959e1a80420
Jose Lopez <josejuventino.z@infosys.com>
2019-01-09 13:36:50 -0800

    Funnels

--- Ritual/AppDelegate.swift
+++ Ritual/AppDelegate.swift
@@ -10,7 +10,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         Fabric.with([Crashlytics.self])
+        
         TrackingManager.initializeMixpanel()
+        TrackingManager.trackEvent(name: "App Opened")
         
         return true
     }
